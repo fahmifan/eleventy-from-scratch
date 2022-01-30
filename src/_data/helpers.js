3c9131b0519c2dceed7d5e481ab24ac1ed2337cc
fahmi.irfan <fahmi.irfan@kumparan.com>
2022-01-30 20:58:36 +0700

    part 7

---
+++ src/_data/helpers.js
@@ -0,0 +1,22 @@
+module.exports = {
+    /**
+   * Returns back some attributes based on whether the
+   * link is active or a parent of an active item
+   *
+   * @param {String} itemURL The link in question
+   * @param {String} pageURL The page context
+   * @returns {String} The attributes or empty
+   */
+    getLinkActiveState(itemURL, pageURL) {
+        let response = ''
+        if (itemURL === pageURL) {
+            response = ' aria-current="page"'
+        }
+
+        if (itemURL.length > 1 && pageURL.indexOf(itemURL) === 0) {
+            response += ' data-state="active"';
+        }
+
+        return response
+    },
+}
