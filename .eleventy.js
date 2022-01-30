a1d0f5e1d113822a0abe7d9f53af30737281e1ba
fahmi.irfan <fahmi.irfan@kumparan.com>
2022-01-30 21:13:52 +0700

    part 8

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,22 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order')
+
 module.exports = config => {
     config.addPassthroughCopy('./src/images/');
+
+    // Returns work items, sorted by display order
+    config.addCollection('work', collection => {
+        const res = collection.getFilteredByGlob('./src/work/*.md')
+        return sortByDisplayOrder(res);
+    });
+
+    // Returns work items, sorted by display order then filtered by featured
+    config.addCollection('featuredWork', collection => {
+        const res = collection.getFilteredByGlob('./src/work/*.md')
+        return sortByDisplayOrder(res)
+            .filter(
+                x => x.data.featured
+            );
+    });
 
     return {
         dir: {
